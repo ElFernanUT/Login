61dee63fda2b5179c222efe3b934f3c33ce95808
AlejandroVelazquezRuizTi03SM-22 <alejandrovr.ti22@utsjr.edu.mx>
2023-10-27 22:05:35 -0600

    primer commit

---
+++ bd/conexion.js
@@ -0,0 +1,15 @@
+var admin=require("firebase-admin");
+var keys=require("../keys.json");
+
+admin.initializeApp({
+    credential:admin.credential.cert(keys)
+});
+var cuenta=admin.firestore();
+
+var conexionUsuarios=cuenta.collection("ejemploBD");
+var conexionProductos=cuenta.collection("Productos");
+var conexionProductos=cuenta.collection("Usuarios");
+module.exports={
+conexionProductos,
+conexionUsuarios
+};
